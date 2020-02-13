b3090f8729ed928e732eb195158328fbaf7e09e7
Jihyo Kim <kimjihyo0325@gmail.com>
2020-02-12 20:12:23 -0800

    initial commit

---
+++ app/src/main/java/ca/bcit/kim/wilson/comp_3717_android_assignment1/MainActivity.java
@@ -0,0 +1,14 @@
+package ca.bcit.kim.wilson.comp_3717_android_assignment1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
