9ee0363ad516976ac5717de1dac18a89254a38e1
spencerwilson123321 <swilson176@my.bcit.ca>
2020-02-13 11:11:51 -0800

    Added functionality to Search button

--- app/src/main/java/ca/bcit/kim/wilson/comp_3717_android_assignment1/MainActivity.java
+++ app/src/main/java/ca/bcit/kim/wilson/comp_3717_android_assignment1/MainActivity.java
@@ -2,14 +2,35 @@ package ca.bcit.kim.wilson.comp_3717_android_assignment1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    @Override
+    public void onClick(View view){
+        Button search_button = findViewById(R.id.search_button);
+        EditText search_bar = findViewById(R.id.search_bar);
+        if ((view == search_button && !search_bar.getText().toString().isEmpty())){
+            TextView test = findViewById(R.id.header_text);
+            // Place JSON code here
+            Intent i = new Intent(this, SearchResults.class);
+            startActivity(i);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.search_button);
+        button.setOnClickListener(this);
+
     }
 
 
