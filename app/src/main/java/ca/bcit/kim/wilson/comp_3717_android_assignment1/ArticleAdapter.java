d64598d025cb9842736452fc4f13488b758d02bf
Jihyo Kim <kimjihyo0325@gmail.com>
2020-02-13 19:14:10 -0800

    implemented details page

--- app/src/main/java/ca/bcit/kim/wilson/comp_3717_android_assignment1/ArticleAdapter.java
+++ app/src/main/java/ca/bcit/kim/wilson/comp_3717_android_assignment1/ArticleAdapter.java
@@ -2,11 +2,14 @@ package ca.bcit.kim.wilson.comp_3717_android_assignment1;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
+
+import com.google.gson.Gson;
 
 import java.util.ArrayList;
 
@@ -22,7 +25,7 @@ public class ArticleAdapter extends ArrayAdapter<Article> {
     public View getView(int position, View convertView, ViewGroup parent) {
         final Activity activity = (Activity) context;
         // Get the data item for this position
-        Article article = getItem(position);
+        final Article article = getItem(position);
         // Check if an existing view is being reused, otherwise inflate the view
         if (convertView == null) {
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.layout_article_list_adapter, parent, false);
@@ -31,8 +34,20 @@ public class ArticleAdapter extends ArrayAdapter<Article> {
         if (article.getTitle() != null) {
             textView.setText(article.getTitle());
         } else {
-            textView.setText("NULL");
+            textView.setText(R.string.default_title);
         }
+
+        convertView.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(context, DetailsActivity.class);
+                Gson gson = new Gson();
+                String articleInStr = gson.toJson(article, Article.class);
+                i.putExtra(SearchResults.ARTICLE, articleInStr);
+                context.startActivity(i);
+            }
+        });
 
         // Return the completed view to render on screen
         return convertView;
